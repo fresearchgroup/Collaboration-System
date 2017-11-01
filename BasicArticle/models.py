dbdd203232e76836eb86e4d83e3c865c25177d04
abhisgithub <abhijitbonik@hotmail.com>
2017-11-01 17:33:40 +0530

    initial models created

--- BasicArticle/models.py
+++ BasicArticle/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
