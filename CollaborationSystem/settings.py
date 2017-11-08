3acfc6e1a3b8fef6b022d81680b1656b1fac968c
abhisgithub <abhijitbonik@hotmail.com>
2017-11-08 18:57:25 +0530

    Mysql engine configured

--- CollaborationSystem/settings.py
+++ CollaborationSystem/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'CollaborationSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
