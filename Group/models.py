39b076f48c52b511ceb25bb90c3747404131488c
abhisgithub <abhijitbonik@hotmail.com>
2017-11-10 14:42:32 +0530

    group models defined

--- Group/models.py
+++ Group/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group as Roles
+from communities.models import Community
 
 # Create your models here.
+
+class Group(models.Model):
+	name = models.CharField(max_length=100)
+	desc = models.TextField()
+	created_by = models.ForeignKey(User, related_name='group')
+	community = models.ForeignKey(Community,null=True,  related_name='group')
+	visibility = models.BooleanField()
+
+	def __str__(self):
+		return self.name
