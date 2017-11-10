fd4b5af6d199de1b3d46f31e1627f44ff5b85395
abhisgithub <abhijitbonik@hotmail.com>
2017-11-10 12:17:52 +0530

    articles in community added

--- Community/models.py
+++ Community/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group as Roles
+from BasicArticle.models import Articles
 
 # Create your models here.
 class Community(models.Model):
@@ -15,3 +16,8 @@ class CommunityMembership(models.Model):
 	user = models.ForeignKey(User, related_name='communitymembership')
 	community = models.ForeignKey(Community, related_name='communitymembership')
 	role = models.ForeignKey(Roles, null=True, related_name='communitymembership')
+
+class CommunityArticles(models.Model):
+	article = models.ForeignKey(Articles, related_name='communityarticles')
+	user = models.ForeignKey(User, related_name='communityarticles')
+	community = models.ForeignKey(Community, related_name='communityarticles')
