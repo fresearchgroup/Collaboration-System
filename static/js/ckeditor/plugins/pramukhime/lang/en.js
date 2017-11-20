2501940143e9b5c0c9631b233a39dd7eb429c9ed
abhisgithub <abhijitbonik@hotmail.com>
2017-11-20 16:59:38 +0530

    static location added

---
+++ static/js/ckeditor/plugins/pramukhime/lang/en.js
@@ -0,0 +1,20 @@
+
+CKEDITOR.plugins.setLang('pramukhime', 'en',
+{
+    pramukhime_title: 'PramukhIME',
+	pramukhimehelp_desc: 'PramukhIME Typing Help',
+	pramukhimetogglelanguage_desc: 'Last Selected Language',
+	pramukhime_languages_title: 'Languages',
+	reset_settings_title: 'Reset PramukhIME Settings',
+	reset_settings_desc: 'Are you sure you want to reset PramukhIME settings?',
+	settings_title: 'PramukhIME Settings',
+	help_title: "PramukhIME Help",
+	help_quick_help: "Quick Typing Help",
+	help_detailed_help: "Detailed Typing Help",
+	help_about: "About",
+	help_about_ime: "<strong>PramukhIME 4.0.0</strong><br /><br />CKEditor Supported Version: 4.x<br /><br />PramukhIME plugin is a platform independent web based typing plugin derived from <a href=\"http://www.vishalon.net/pramukhime/javascript-library\" target=\"_blank\">PramukhIME <br />javascript library</a>.It is developed by <a href=\"http://www.vishalon.net\" target=\"_blank\">Vishal Monpara</a>. Please visit the website to get latest version <br />of this plugin as well as other resources.<br /><br />Copyright &copy; Vishal Monpara<br /><br />",
+    pramukhime:
+    {
+		english: 'English'
+    }
+});
---
+++ static/js/ckeditor/plugins/pramukhime/lang/en_pramukhindic.js
@@ -0,0 +1,43 @@
+// Following function is created on my own to preserve kb specific translation.
+CKEDITOR.plugins.addLang('pramukhime', 'en',
+{
+	pramukhindic:
+	{
+		pramukhindic: 'Pramukh Indic',
+		help_quick_help1: "This plugin is used for typing easily into Indian language. It follows 'The way you speak, <br />the way you type' rule. Following image shows which character will be shown by <br />pressing which corresponding English letter. Example for various combination of letters <br />are given only for one character but it is true for each alphabet.",
+		help_quick_help2: ' ',
+		help_about_kb: "<strong>Pramukh Indic Keyboard 4.0.0</strong><br /><br />Pramukh Indic Keyboard is a keyboard plugin developed for <a href=\"http://www.vishalon.net/pramukhime/javascript-library\" target=\"_blank\">PramukhIME</a>. It supports Assamese, <br />Bengali, Bodo, Dogri, Gujarati, Hindi, Kannada, Kashmiri, Konkani, Maithili, Malayalam, <br />Manipuri, Marathi (Devanagari/Modi), Meitei, Nepali, Odia, Punjabi, Sanskrit, Santali (Ol Chiki), <br />Sindhi, Sora, Tamil and Telugu languages. It is developed by <a href=\"http://www.vishalon.net\" target=\"_blank\">Vishal Monpara</a>. Please visit the <br />website to get latest version of this plugin as well as other Indian language resources.<br /><br />Copyright &copy; Vishal Monpara",
+		assamese: 'Assamese',
+		bengali: 'Bengali',
+		bodo: 'Bodo',
+		dogri: 'Dogri',
+		gujarati: 'Gujarati',
+		hindi: 'Hindi',
+		kannada: 'Kannada',
+		kashmiri: 'Kashmiri',
+		konkani: 'Konkani',
+		maithili: 'Maithili',
+		malayalam: 'Malayalam',
+		manipuri: 'Manipuri',
+		marathi: 'Marathi',
+		marathimodi: 'Marathi (Modi)',
+		meitei: 'Meitei',
+		nepali: 'Nepali',
+		odia: 'Odia',
+		punjabi: 'Punjabi',
+		sanskrit: 'Sanskrit',
+		santali: 'Santali',
+		sindhi: 'Sindhi',
+		sora: 'Sora',
+		tamil: 'Tamil',
+		telugu: 'Telugu',
+		typing_rule: 'Typing Rule', // Needed for temporary placeholder
+		hindi_typing_rule: 'Hindi Typing Rule',
+		kannada_typing_rule: 'Kannada Typing Rule',
+		kashmiri_typing_rule: 'Kashmiri Typing Rule',
+		settings: 'Pramukh Indic Keyboard Settings',
+		digit_in_english: 'Digit in English (for all Pramukh Indic languages)',
+		panchamakshar: 'Panchamakshar',
+		anusvar: 'Anusvar'	
+	}
+});
