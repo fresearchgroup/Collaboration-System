bc986a5e255a2d989483b49f0936c6ca1c373b77
firuza <firya87@gmail.com>
2017-11-23 21:46:14 +0530

    In a group users can subscribe and unsubscribe

--- Group/views.py
+++ Group/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
-from .models import Group
-
-
+from .models import Group, GroupMembership
 
 def create_group(request):
 	if request.method == 'POST':
@@ -17,14 +15,33 @@ def create_group(request):
 		return group
 
 def group_view(request, pk):
-    try:
-        group = Group.objects.get(pk=pk)
-    except Group.DoesNotExist:
-        raise Http404
-    return render(request, 'groupview.html', {'group': group})
+	try:
+		group = Group.objects.get(pk=pk)
+		uid = request.user.id
+		membership = GroupMembership.objects.get(user=uid, group=group.pk)
+	except GroupMembership.DoesNotExist:
+		membership = 'FALSE'
+	subscribers = GroupMembership.objects.filter(group = pk).count()
+	contributors = GroupMembership.objects.filter(group = pk)
+	return render(request, 'groupview.html', {'group': group, 'membership':membership, 'subscribers':subscribers, 'contributors':contributors})
 
 def group_subscribe(request):
-	return redirect('login')
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			gid = request.POST['gid']
+			group = Group.objects.get(pk=gid)
+			user = request.user
+			obj = GroupMembership.objects.create(user=user, group=group)
+			return redirect('group_view',pk=gid)
+		return render(request, 'groupview.html')
+	else:
+		return redirect('login')
 
 def group_unsubscribe(request):
-	return redirect('login')
+	if request.method == 'POST':
+		gid = request.POST['gid']
+		group = Group.objects.get(pk=gid)
+		user = request.user
+		obj = GroupMembership.objects.filter(user=user, group=group).delete()
+		return redirect('group_view',pk=gid)
+	return render(request, 'groupview.html')
