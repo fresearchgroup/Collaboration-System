533091c016ea42b9718bd6d5a453878828d7df1d
firuza <firya87@gmail.com>
2017-11-30 14:01:04 +0530

    listed top contributors in the group

--- Group/views.py
+++ Group/views.py
@@ -25,8 +25,9 @@ def group_view(request, pk):
 		membership = 'FALSE'
 	subscribers = GroupMembership.objects.filter(group = pk).count()
 	articles = GroupArticles.objects.filter(group = pk)
+	users = GroupArticles.objects.raw('select  u.id,username from auth_user u join Group_grouparticles g on u.id = g.user_id where g.group_id=%s group by u.id order by count(*) desc limit 2;', [pk])
 	contributors = GroupMembership.objects.filter(group = pk)
-	return render(request, 'groupview.html', {'group': group, 'membership':membership, 'subscribers':subscribers, 'contributors':contributors, 'articles':articles})
+	return render(request, 'groupview.html', {'group': group, 'membership':membership, 'subscribers':subscribers, 'contributors':contributors, 'articles':articles, 'users':users})
 
 def group_subscribe(request):
 	if request.user.is_authenticated:
