28209eca14b7b4a9124bd994429c307c932f3b01
firuza <firya87@gmail.com>
2017-12-05 15:58:59 +0530

    Resolved issue for creating group

--- Community/views.py
+++ Community/views.py
@@ -78,11 +78,10 @@ def community_group(request):
 			if status=='1':
 				group = create_group(request)
 				obj = CommunityGroups.objects.create(group=group, user=request.user, community=community)
-				return redirect('group_view', group.pk)
+				return redirect('group_view', cid, group.pk)
 			else:
 				return render(request, 'new_group.html', {'community':community, 'status':1})
 		else:
 			return redirect('home')
 	else:
 		return redirect('login')
-
