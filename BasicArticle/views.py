06a7869f2ff2f20236f9fadc3e7e3eb802a40829
abhisgithub <abhijitbonik@hotmail.com>
2018-01-09 21:55:06 +0530

    added fixtures for workflow

--- BasicArticle/views.py
+++ BasicArticle/views.py
@@ -219,31 +219,3 @@ def article_watch(request, article):
 
 class SimpleModelHistoryCompareView(HistoryCompareDetailView):
     model = Articles
-
-
-def workflow(pk, current_state, to_state,  role, belongs_to):
-	article = Articles.objects.get(pk=pk)
-	if belongs_to == 'community':
-		if role == 'publisher':
-			current_state = States.objects.get(name=current_state)
-			to_state = States.objects.get(name=to_state)
-			if article.state == to_state:
-				return 
-			elif article.state == 'publish':
-				return 
-			else:
-				try:
-					transitions = Transitions.objects.get(from_state=current_state, to_state=to_state)
-					article.state = to_state
-					article.save(update_fields=["state"])
-					return 
-				except Transitions.DoesNotExist:
-					return 
-
-		elif role == 'author':
-			return 
-
-	elif belongs_to == 'group':
-		return 
-
-
