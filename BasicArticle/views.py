3a09fd1c81af011703c38afb7eae2cf335c017fd
firuza <firya87@gmail.com>
2018-01-18 15:22:14 +0000

    resolved issue for image in edit article

--- BasicArticle/views.py
+++ BasicArticle/views.py
@@ -84,8 +84,11 @@ def edit_article(request, pk):
 					article = Articles.objects.get(pk=pk)
 					article.title = form.cleaned_data.get('title')
 					article.body = form.cleaned_data.get('body')
-					article.image = request.FILES['article_image']
-					article.save(update_fields=["title","body","image"])
+					try:
+						article.image = request.FILES['article_image']
+						article.save(update_fields=["title","body","image"])
+					except:
+						article.save(update_fields=["title","body"])
 					return redirect('article_view',pk=article.pk)
 			else:
 				article = Articles.objects.get(pk=pk)
@@ -108,8 +111,11 @@ def edit_article(request, pk):
 						article.state = to_state
 					article.title = title
 					article.body = body
-					article.image = request.FILES['article_image']
-					article.save(update_fields=["title","body", "image", "state"])
+					try:
+						article.image = request.FILES['article_image']
+						article.save(update_fields=["title","body", "image", "state"])
+					except:
+						article.save(update_fields=["title","body", "state"])
 				except Transitions.DoesNotExist:
 					message = "transition doesn' exist"
 				except States.DoesNotExist:
