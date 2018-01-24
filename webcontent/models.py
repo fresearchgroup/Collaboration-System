01b1ed8c7e6b003bc12d66843009aa8b7120d4ad
firuza <firya87@gmail.com>
2018-01-24 11:46:51 +0000

    modified feedback ORM

--- webcontent/models.py
+++ webcontent/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Feedback(models.Model):
 	title = models.CharField(max_length=100)
 	body = models.TextField()
+	user = models.ForeignKey(User, related_name='user')
+	provided_at = models.DateTimeField(auto_now_add=True)
 
 class Faq(models.Model):
 	category = models.CharField(max_length=100)
