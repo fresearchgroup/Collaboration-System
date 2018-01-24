fef2b7200c4fcff2edf488d19cf99ad8a396d099
firuza <firya87@gmail.com>
2018-01-24 07:57:16 +0000

    Passing groups of same community

--- Group/views.py
+++ Group/views.py
@@ -50,7 +50,8 @@ def group_view(request, pk):
 	pubarticlescount = len(list(pubarticles))
 	users = GroupArticles.objects.raw('select  u.id,username from auth_user u join Group_grouparticles g on u.id = g.user_id where g.group_id=%s group by u.id order by count(*) desc limit 2;', [pk])
 	contributors = GroupMembership.objects.filter(group = pk)
-	return render(request, 'groupview.html', {'group': group, 'communitymembership':communitymembership,'membership':membership, 'subscribers':subscribers, 'contributors':contributors, 'users':users, 'community':community,'message':message,'pubarticles':pubarticles,'pubarticlescount':pubarticlescount})
+	othergroups = CommunityGroups.objects.filter(community = community.community.pk)
+	return render(request, 'groupview.html', {'group': group, 'communitymembership':communitymembership,'membership':membership, 'subscribers':subscribers, 'contributors':contributors, 'users':users, 'community':community,'message':message,'pubarticles':pubarticles,'pubarticlescount':pubarticlescount, 'othergroups':othergroups})
 
 def group_subscribe(request):
 	if request.user.is_authenticated:
