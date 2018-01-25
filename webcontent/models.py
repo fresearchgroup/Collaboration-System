a71eb0e220a92df6ec26dfa80ea5bb0b07ae6456
abhisgithub <abhijitbonik@hotmail.com>
2018-01-25 21:23:22 +0530

    faq category models and json upload added

--- webcontent/models.py
+++ webcontent/models.py
@@ -9,7 +9,12 @@ class Feedback(models.Model):
 	user = models.ForeignKey(User, null=True, related_name='fuser')
 	provided_at = models.DateTimeField(null=True, auto_now_add=True)
 
+class FaqCategory(models.Model):
+	name = models.CharField(max_length=50)
+	desc = models.TextField()
+
+
 class Faq(models.Model):
-	category = models.CharField(max_length=50)
+	category = models.ForeignKey(FaqCategory, null=True, related_name='faqcategory')
 	question = models.CharField(max_length=200)
 	answer = models.TextField()
