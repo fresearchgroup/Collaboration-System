1d75ac7992a64b6d39bcc993c6de7cc893b4f960
abhisgithub <abhijitbonik@hotmail.com>
2018-02-15 17:21:12 +0530

    if login will redirect to userdasbord

--- CollaborationSystem/urls.py
+++ CollaborationSystem/urls.py
@@ -36,7 +36,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^$', user_views.home, name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^signup/$', user_views.signup, name ='signup' ),
     url(r'^', include(router.urls)),
