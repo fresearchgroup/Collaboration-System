e3e762b55cdad1a9072a7dc2ddd40b5c030931b6
abhisgithub <abhijitbonik@hotmail.com>
2018-02-27 17:01:53 +0530

    random file for user image fix

--- UserRolesPermission/helpers.py
+++ UserRolesPermission/helpers.py
@@ -2,11 +2,7 @@ from django.utils.deconstruct import deconstructible
 import os, uuid
 
 
-class RandomFileName(object):
-    def __init__(self, path):
-        self.path = os.path.join(path, "%s%s")
-
-    def __call__(self, _, filename):
-        # @note It's up to the validators to check if it's the correct file type in name or if one even exist.
-        extension = os.path.splitext(filename)[1]
-        return self.path % (uuid.uuid4(), extension)
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('profile', filename)
