62ea595fa0d0fcdb81876a7e9bbc661077827c3d
NithinSethumadhavan <tv.nithin.s@gmail.com>
2018-03-01 15:55:52 +0530

    Added AnsibleX

--- Community/models.py
+++ Community/models.py
@@ -37,10 +37,6 @@ class CommunityArticles(models.Model):
 	
 	def get_absolute_url(self):
 		from django.urls import reverse
-		return reverse('article_view', kwargs={'pk': self.id})
-
-	def get_absolute_url(self):
-		from django.urls import reverse
 		return reverse('article_view', kwargs={'pk': self.article_id})
 
 class CommunityGroups(models.Model):
