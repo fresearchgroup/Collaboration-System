927820e7e50f9d88ff075207c2073e070e7dd406
rushimanjrekar <rushikesh.manjrekar10@gmail.com>
2018-03-07 20:11:14 +0530

    test changes

--- Community/tests.py
+++ Community/tests.py
@@ -1,3 +1,13 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
+from Community.views import community_view
+from .models import Community
 
-# Create your tests here.
+
+class communityviewtest(TestCase):
+	def test_community_view_success_status_code(self):
+		url = reverse('community_view', kwargs={'pk': 1})
+		response =self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+	
