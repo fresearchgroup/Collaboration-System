6ee106d23ea272e2f359131637c2cb517812802d
kamleshrajpoot <rajkamlesh3693@gmail.com>
2018-03-15 21:50:00 +0530

    testing in Group app

--- Group/tests.py
+++ Group/tests.py
@@ -28,17 +28,56 @@ class Grouptest(TestCase):
     
 
 
-    def test_update_group_info_valid_post_data(self,kwargs={'pk': 1}):
-        url = reverse('update_group_info')
+    def test_update_group_info_valid_post_data(self):
+        url = reverse('update_group_info',kwargs={'pk': 1})
         data = {
             'name':'Education Centre',
             'desc':'this is for better education',
-            'visibility': '1'
+            'visibility': 'public'
         }
         response = self.client.post(url, data)
         self.assertFalse(Group.objects.exists())
     
 
+    def test_group_unsubscribe_valid_post_data(self):
+        url = reverse('group_unsubscribe')
+        data = {
+            'gid ':'1',
+            'user':'kamleshsisodiya'
+        }
+        response = self.client.post(url, data)
+        self.assertFalse(GroupMembership.objects.exists())
 
+    def test_manage_group_valid_post_data(self):
+        url = reverse('manage_group',kwargs={'pk': 2})
+        data = {
+            'user':'kamleshsisodiya',
+            'role':'author'
+        }
+        response = self.client.post(url, data)
+        self.assertFalse(GroupMembership.objects.exists())
+
+    def test_group_article_create_valid_post_data(self):
+        url = reverse('group_article_create')
+        data={
+        'gid':'1',
+        'status':'publishable',
+        'group':'Education Centre'
+        }
+        response = self.client.post(url, data)
+        self.assertFalse(Group.objects.exists())
+
+    def group_view_success_status_code(self):
+        url = reverse('group_view', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+
+    def group_view_not_found_status_code(self):
+        url = reverse('group_view', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    
 
 
