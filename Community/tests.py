a5e1544e2d3e07c30beae1b1bbba4dd80eddd5e4
rushimanjrekar <rushikesh.manjrekar10@gmail.com>
2018-03-16 20:04:21 +0530

    additional changes on community testcase

--- Community/tests.py
+++ Community/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .views import display_communities, request_community_creation, create_community, community_view
-from .models import Community, RequestCommunityCreation
+from .models import Community, RequestCommunityCreation, CommunityArticles, CommunityGroups
 
 class DisplayCommunityTest(TestCase):
     def test_display_community_view_status_code(self):
@@ -11,12 +11,75 @@ class DisplayCommunityTest(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
+    def test_display_communities_url_resloves_display_communities_view(self):
+    	view = resolve('/communities/')
+    	self.assertEquals(view.func, display_communities) 
+
+
+class CommunityViewTest(TestCase):
+	def community_view_success_status_code(self):
+		url = reverse('community_view', kwargs={'pk': 1})
+		response =self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+	
+	def test_community_view_url_resolves_community_view(self):
+		view = resolve('/community-view/1/')
+		self.assertEquals(view.func, community_view) 
+
+
+class CommunitySubscribeTest(TestCase):
+	def test_community_subscribe_valid_post(self):
+		url = reverse('community_subscribe')
+		data = {
+			'cid': '1' 
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(Community.objects.exists())
+
+	def test_community_subscribe_url_resolves_community_subscribe_view(self):
+		view = resolve('/community-subscribe/')
+		self.assertEquals(view.func, community_subscribe) 
+
+
+class CommunityUnsubscribeTest(TestCase):
+	def test_community_unsubscribe_valid_post(self):
+		url = reverse('community_unsubscribe')
+		data = {
+			'cid': '1' 
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(Community.objects.exists())
+
+	def test_community_unsubscribe_url_resolves_community_unsubscribe_view(self):
+		view = resolve('/community-unsubscribe/')
+		self.assertEquals(view.func, community_unsubscribe)
+
+
+class CommunityArticleCreateTest(TestCase):
+	def test_community_article_create_valid_post(self):
+		url = reverse('community_article_create')
+		data = {
+			'cid': '1' 
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(CommunityArticles.objects.exists())
+
+
+class CommunityGroupTest(TestCase):
+	def test_community_group_valid_post(self):
+		url = reverse('community_group')
+		data = {
+			'cid': '1' 
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(CommunityGroups.objects.exists())
+
     
 class RequestCommunityCreationTest(TestCase):
 	def test_request_community_creation_status(self):
 		url = reverse('request_community_creation')
-		response = self.client.get(url)
-		self.assertEquals(response.status_code, 302)
+		self.response = self.client.get(url)
+		self.assertEquals(self.response.status_code, 302)
 
 	def test_request_community_creation_valid_post(self):
 		url = reverse('request_community_creation')
@@ -27,32 +90,23 @@ class RequestCommunityCreationTest(TestCase):
 		response = self.client.post(url, data)
 		self.assertFalse(RequestCommunityCreation.objects.exists())
 
-	#def test_request_community_creation_url_resolves_request_community_creation_view(self):
-	#	view = resolve('request_community_creation/')
-	#	self.assertEquals(view.func, request_community_creation)   
+	def test_request_community_creation_url_resolves_request_community_creation_view(self):
+		view = resolve('/request_community_creation/')
+		self.assertEquals(view.func, request_community_creation) 
 
-
-class CreateCommunityTest(TestCase):
-	def test_create_community_status(self):
-		url = reverse('create_community')
-		response = self.client.get(url)
-		self.assertEquals(response.status_code, 302)
-
-
-
-class communityviewtest(TestCase):
-	def community_view_success_status_code(self):
-		url = reverse('community_view', kwargs={'pk': 1})
-		response =self.client.get(url)
-		self.assertEquals(response.status_code, 200)
 	
-		
-	#def test_community_view_url_resolves_community_view(self):
-	#	view = resolve('community-view/1/')
-	#	self.assertEquals(view.func, community_view)
+class HandleCommunityCreationRequestsTest(TestCase):
+	def test_handle_community_creation_requests_valid_post(self):
+		url = reverse('handle_community_creation_requests')
+		data = {
+			'name': 'Indian Festivals',
+			'category': 'festival'
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(Community.objects.exists())  
 
-class manage_community(TestCase):
-	
+
+class ManageCommunityTest(TestCase):
 	def test_manage_community_valid_post(self):
 		url = reverse('manage_community', kwargs={'pk': 2})
 		data = {
@@ -61,21 +115,9 @@ class manage_community(TestCase):
 		}
 		response = self.client.post(url, data)
 		self.assertFalse(Community.objects.exists())
-	
-class handle_community_creation_requests(TestCase):
-	
-	def test_handle_community_creation_requests_valid_post(self):
-		url = reverse('handle_community_creation_requests')
-		data = {
-			'name': 'Indian Festivals',
-			'category': 'festival'
-		}
-		response = self.client.post(url, data)
-		self.assertFalse(Community.objects.exists())
 
 
-class update_community_info(TestCase):
-	
+class UpdateCommunityInfoTest(TestCase):
 	def test_update_community_info_valid_post(self):
 		url = reverse('update_community_info', kwargs={'pk': 2})
 		data = {
@@ -84,3 +126,29 @@ class update_community_info(TestCase):
 		}
 		response = self.client.post(url, data)
 		self.assertFalse(Community.objects.exists())
+
+
+class CreateCommunityTest(TestCase):
+	def test_create_community_status(self):
+		url = reverse('create_community')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 302)
+
+	def test_create_community_valid_post(self):
+		url = reverse('create_community')
+		data = {
+			'name': 'Indian Festivals',
+			'category': 'festival'
+		}
+		response = self.client.post(url, data)
+		self.assertFalse(Community.objects.exists())
+
+	def test_create_community_url_resolves_request_community_view(self):
+		view = resolve('/create_community/')
+		self.assertEquals(view.func, create_community)
+
+
+#class CommunityContentTest(TestCase):		
+
+
+#class CommunityGroupContentTest(TestCase):
