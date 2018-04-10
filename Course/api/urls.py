3f111376f98141e9dffa1da7922cb5a86a1b0545
abhisgithub <abhijitbonik@hotmail.com>
2018-04-10 14:47:18 +0530

    url modified

--- Course/api/urls.py
+++ Course/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import CourseRUDApiView, CourseCreateApiView
 
 urlpatterns = [
-	url(r'^$', CourseCreateApiView.as_view(), name='cousre-create-api'),
+	url(r'^create$', CourseCreateApiView.as_view(), name='cousre-create-api'),
     url(r'^(?P<pk>\d*)/$', CourseRUDApiView.as_view(), name='cousre-rud-api'),
     ]
