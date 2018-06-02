63e58a15a6b5f56cd36085e5fd14706ffafca883
Kartik Verma <kvs1297@gmail.com>
2018-06-02 19:49:15 +0530

    custom color added for utils

--- utils.py
+++ utils.py
@@ -7,12 +7,14 @@ GREEN = 2
 BLUE = 3
 YELLOW = 4
 MAGENTA = 5
+IMP_COLOR = 6
 
 LEVEL_COLOR = {
+        "IMP": IMP_COLOR,
         "INFO": GREEN,
         "ERROR": RED,
         "WARNING": MAGENTA,
-        "DEBUG": BLUE
+        "DEBUG": BLUE,
         }
 
 COLOR_SEQ = {
@@ -20,27 +22,39 @@ COLOR_SEQ = {
          GREEN: (66, 244, 200),
          BLUE: (33, 28, 188),
          YELLOW: (175, 132, 22),
-         MAGENTA: (255, 0, 255)
-        }
+         MAGENTA: (255, 0, 255),
+         IMP_COLOR: (51, 32, 229),
+        } 
 
-def __appendColor(color):
-    return "\033[38;2;" + str(COLOR_SEQ[color][0]) + ";" + str(COLOR_SEQ[color][1]) + ";" + str(COLOR_SEQ[color][2]) + "m"
+def __appendColor(color, custom = False):
+    if custom == False:
+        return "\033[38;2;" + str(COLOR_SEQ[color][0]) + ";" + str(COLOR_SEQ[color][1]) + ";" + str(COLOR_SEQ[color][2]) + "m"
+    elif custom == True:
+        return "\033[38;2;" + str(color[0]) + ";" + str(color[1]) + ";" + str(color[2]) + "m"
 
 def __attach_time_stamp():
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
     return st
 
-def __formatter(level, msg, logclass):
-    color_code = LEVEL_COLOR[level]
+def __formatter(level, msg, logclass, color_spec_dic = None):
+    color_code = None
+    if color_spec_dic == None:
+        color_code = LEVEL_COLOR[level]
+    else:
+        color_code = color_spec_dic['color_code']
     color_str = __appendColor(color_code)
     timestamp = __attach_time_stamp()
-    fstring = str(color_str) + "[{!s}-[{!s}]:{!s}: ".format(timestamp, level, logclass) + str(msg) + "\033[0m"
-    return fstring
-
-def ilog(logclass, msg, mode = "INFO", imp = False):
+    fstring = str(color_str) + "[{!s}-{!s}]:{!s}: ".format(timestamp, level, logclass) + str(msg) + "\033[0m"
+    return fstring 
+        
+def ilog(logclass, msg, mode = "INFO", imp = False, color_spec_dic = None):
     if ms.DEBUG == True or imp == True:
-        print(__formatter(mode, msg, logclass)) 
+        if imp == True:
+            mode = "IMP"
+            color_spec_dic = None
+        print(__formatter(mode, msg, logclass, color_spec_dic))
+        
 
 
     
