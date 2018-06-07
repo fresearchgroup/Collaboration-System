c9ddbb98d426750b436b007c9a945a95908d5a42
Divyansh Sharma <divyansh9595@gmail.com>
2018-06-07 23:28:25 +0530

    es functionality

---
+++ essearch.py
@@ -0,0 +1,39 @@
+from elasticsearch import Elasticsearch
+from . import settings
+
+class SearchElasticSearch:
+    
+    def __init__(self):
+        self.index = settings.ES_INDEX
+        self.es = Elasticsearch()
+        self.outter_keys = settings.OUTTER_KEYS
+      
+    def build_search_body(search_key_dic):
+        
+        search_keys = search_key_dic.keys()
+        
+        body = {
+            "query" : {
+               "bool" :{
+                   "must":[]
+                }
+            }
+        }
+
+        for key in search_keys:
+            match={"match":{}}
+            if key in self.outter_keys:
+                match["match"].update({key:search_key_dic[key]})
+            else:
+                match["match"].update({'event.'+key:search_key_dic[key]})
+            body["query"]["bool"]["must"].append(match)
+
+        return body    
+                
+    def search_elasticsearch(search_key_dic):
+            body = self.build_search_body(search_key_dic)
+            res =  es.search(index=self.index,body=body)
+            response = []
+            for hit in res['hits']['hits']:
+                response.append(hit['_source'])
+            return response
---
+++ settings.py
@@ -0,0 +1,18 @@
+OUTTER_KEYS = [
+        'server-host',
+        'ip-addres',
+        'session-id',
+        'path-info',
+        'accept-language',
+        'user-agent',
+        'referer',
+        'event-source',
+        'time-stamp',
+        'event_name',
+        'host',
+        '@version',
+        'headers',
+        '@timestamp'
+    ]
+    
+ES_INDEX = 'logstash-2018.06.06'
