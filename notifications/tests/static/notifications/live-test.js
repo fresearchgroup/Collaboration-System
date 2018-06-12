f6c951efda3ddf5c5c9803b9b375857022417670
Kanika Dhiman <dhimankanika17@gmail.com>
2018-06-12 23:47:23 +0530

    removed notifications clone

--- notifications/tests/static/notifications/live-test.js
+++
@@ -1,7 +0,0 @@
-console.log('running tester')
-
-function make_notification() {
-    var r = new XMLHttpRequest();
-    r.open("GET", '/test_make/', true);
-    r.send();
-}
--- temp/notifications/settings.py
+++ temp/notifications/settings.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 CONFIG_DEFAULTS = {
-    'PAGINATE_BY': 10,
+    'PAGINATE_BY': 20,
     'USE_JSONFIELD': False,
     'SOFT_DELETE': False
 }
