a010be34125d9c3b0d653c75fc25e6812a642201
abhisgithub <abhijitbonik@hotmail.com>
2018-06-19 16:02:42 +0530

    api for group created yester day and article published in group yesterdy

---
+++ Group/viewsets.py
@@ -0,0 +1,20 @@
+from .models import Group, GroupArticles
+from .serializers import GroupSerializer, GroupArticlesSerializer
+from rest_framework import generics
+import datetime, time
+# Create your views here.
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(1)
+yesterday_start = datetime.datetime.combine(yesterday, datetime.time())
+yesterday_end = datetime.datetime.combine(today, datetime.time())
+
+class GroupViewSet(generics.ListAPIView):
+	queryset = Group.objects.filter(created_at__range=[yesterday_start, yesterday_end])
+	serializer_class = GroupSerializer
+
+
+class GroupArticleViewsets(generics.ListAPIView):
+	serializer_class = GroupArticlesSerializer
+
+	def get_queryset(self):
+		return GroupArticles.objects.filter(article__state__name='publish', article__published_on__range=[yesterday_start, yesterday_end])
