8560ad48cb4229ed6e8dc1cf4d107da266299a65
abhisgithub <abhijitbonik@hotmail.com>
2018-06-19 15:48:14 +0530

    communities apis updated

--- Community/serializers.py
+++ Community/serializers.py
@@ -4,30 +4,33 @@ from .models import Community, CommunityArticles
 class CommunitySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Community
-		fields = ('name', 'desc','category', 'created_at', 'created_by')
+		fields = ('id', 'name', 'desc','category', 'created_at', 'created_by')
 
 
 
 class CommunityArticlesSerializer(serializers.ModelSerializer):
+	articleid = serializers.ReadOnlyField(source='article.id')
 	title = serializers.ReadOnlyField(source='article.title')
 	body = serializers.ReadOnlyField(source='article.body')
 	created_by = serializers.ReadOnlyField(source='article.created_by.username')
 	created_at = serializers.ReadOnlyField(source='article.created_at')
 	published_on = serializers.ReadOnlyField(source='article.published_on')
 	state = serializers.ReadOnlyField(source='article.state.name')
-	cname = serializers.ReadOnlyField(source='community.name')
+	communityname = serializers.ReadOnlyField(source='community.name')
+	communityid = serializers.ReadOnlyField(source='community.id')
 
 	class Meta:
 		model = CommunityArticles
 		fields = [
 
-			'article',
+			'articleid',
 			'title',
 			'body',
 			'created_by',
 			'created_at',
 			'published_on',
 			'state',
-			'cname'
+			'communityname',
+			'communityid'
 
 		]
