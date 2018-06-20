80c0a51cee8293f4b5f661b81c8b29f787e5b9f3
Kartik Verma <kvs1297@gmail.com>
2018-06-20 10:08:29 +0530

    integrated code with elastic search

--- eventlog/api/essearch.py
+++ eventlog/api/essearch.py
@@ -7,6 +7,7 @@
 
 from elasticsearch import Elasticsearch
 from . import settings
+from .. import utils
 
 class SearchElasticSearch:
     
@@ -43,7 +44,7 @@ class SearchElasticSearch:
          }
 
         must_keys = search_key_dic['request_keys'].keys()
-
+        utils.ilog(self.LOG_CLASS, "Entered request keys phase...", mode="DEBUG")
         for key in must_keys:
             match={"match":{}}
             if key in self.outter_keys:
@@ -52,23 +53,27 @@ class SearchElasticSearch:
                 match["match"].update({'event.'+key:search_key_dic['request_keys'][key]})
             body["query"]["bool"]["must"].append(match)
 
+        utils.ilog(self.LOG_CLASS, "Entered paging keys phase...", mode="DEBUG")
         if "paging" in search_key_dic.keys():
             body["from"] = search_key_dic["paging"]["from"]
             body["size"] = search_key_dic["paging"]["size"]
            
+        utils.ilog(self.LOG_CLASS, "Entered fields keys phase...", mode="DEBUG")
         if "fields" in search_key_dic.keys():
               field_list =search_key_dic["fields"]
               for field in field_list:
                    body["_source"]["includes"].append(field)
         
+        utils.ilog(self.LOG_CLASS, "Entered sorts keys phase...", mode="DEBUG")
         if "sorts" in search_key_dic.keys():
             for item in search_key_dic["sorts"]:
-                 for key in item:
+                 for key in list(item.keys()):
                     if key in self.outter_keys:
-                         body["sort"].append({key+'.keyword':{"order":item[key]}})
+                         body["sort"].append({str(key)+'.keyword':{"order":item[key]}})
                     else:
-                         body["sort"].append({'event.'+key+'.keyword':{"order":item[key]}})
+                         body["sort"].append({'event.'+str(key)+'.keyword':{"order":item[key]}})
 
+        utils.ilog(self.LOG_CLASS, "Entered filters keys phase...", mode="DEBUG")
         if "filters" in search_key_dic.keys():
             items = search_key_dic["filters"]
             for item in items:
@@ -78,32 +83,34 @@ class SearchElasticSearch:
                 else:
                      body["query"]["bool"]["filter"].append({"term":{'event.'+key:item[key]}})
         
+        utils.ilog(self.LOG_CLASS, "Entered aggregation keys phase...", mode="DEBUG")
         if "agg_keys" in search_key_dic.keys():
             items = search_key_dic["agg_keys"]
             for item in items:
+                utils.ilog(self.LOG_CLASS, "Inserting key: {!s}".format(item), mode="DEBUG")
                 key = list(item.keys())[0]
                 main_dic={}
-                for key in self.outter_keys:
-                    dic = {"field":item+'.keyword'}
+                if key in self.outter_keys:
+                    dic = {"field":str(key)+'.keyword'}
                     main_dic.update({item[key]:dic})
-                body["args"].update({item+'_agg':main_dic})
-            else:
-                key = list(item.keys())[0]
-                main_dic={}
-                for key in self.outter_keys:
-                    dic = {"field":'event.'+item+'.keyword'}
+                else:
+                    dic = {"field":'event.'+key+'.keyword'}
                     main_dic.update({item[key]:dic})
-                body["args"].update({item+'_agg':main_dic})
+                body["aggs"].update({key+'_agg':main_dic})
 
+        utils.ilog(self.LOG_CLASS, "Entered time range keys phase...", mode="DEBUG")
         if "time_range" in search_key_dic.keys():        
             body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"gte":search_key_dic['time_range']['after']})
             body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"lte":search_key_dic['time_range']['before']})
+        
+        utils.ilog(self.LOG_CLASS, "body format: {!s}".format(body), mode="DEBUG")
 
         return body    
                 
     
     def search_elasticsearch(self,search_key_dic):
             response = {}  
+            utils.ilog(self.LOG_CLASS, "Running search key", mode="DEBUG")
             try:
                 body = self.build_search_body(search_key_dic)     
                 res =  self.es.search(index=self.index,body=body)
--- eventlog/api/helpers.py
+++ eventlog/api/helpers.py
@@ -12,7 +12,7 @@ def extract_time_keys(request):
     tm = None
     bef_tm = None
     try:
-        val = request.GET.__getitem__('before')
+        val = dict(request.GET)['before']
         tm = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S')
         bef_tm = tm
         tm = tm.strftime('%Y-%m-%d %H:%M:%S')
@@ -26,7 +26,7 @@ def extract_time_keys(request):
     tm = None
     utils.ilog(LOG_CLASS, "Before time is: {!s}".format(bef_tm), mode="DEBUG")
     try:
-        val = request.GET.__getitem__('after')
+        val = dict(request.GET)['after']
         tm = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S')
         tm = tm.strftime('%Y-%m-%d %H:%M:%S')
     except KeyError as e:
@@ -41,7 +41,7 @@ def extract_field_keys(request):
     to_check = settings.OUTTER_KEYS
     to_check.append('event')
     try:
-        val = request.GET.__getitem__('fields')
+        val = dict(request.GET)['fields']
         dic['fields'] = val
     except KeyError as e:
         for key in settings.OUTTER_KEYS:
@@ -55,13 +55,13 @@ def extract_field_keys(request):
 def extract_paging_keys(request):
     dic = {}
     try:
-        val = int(request.GET.__getitem__('start'))
+        val = int(dict(request.GET)['start'])
         dic['from'] = val
     except KeyError as e:
         dic['from'] = 0
 
     try:
-        val = int(request.GET.__getitem__('limit'))
+        val = int(dict(request.GET)['limit'])
         dic['size'] = val
     except KeyError as e:
         dic['size'] = 5
@@ -74,9 +74,10 @@ def extract_sorting_keys(request):
                 ]
         }
     try:
-        val = request.GET.__getitem__('sorts')
-        for i in range(len(val)):
-            skey = val[i]
+        val = dict(request.GET)['sort']
+        utils.ilog(LOG_CLASS, "Sort keys are: {!s}".format(val), mode="DEBUG")
+        for skey in val:
+            utils.ilog(LOG_CLASS, "parsing the sort strings: {!s}".format(skey), mode="DEBUG")
             skey = skey.strip()
             if skey[0] == '-':
                 dic['sort_keys'].append({
@@ -87,7 +88,7 @@ def extract_sorting_keys(request):
                         '{!s}'.format(skey): 'asc'
                     })
     except KeyError as e:
-        dic['sort_keys'].append({'timestamp': 'desc'})
+        dic['sort_keys'].append({'time-stamp': 'desc'})
     return  dic
 
 def extract_filter_keys(request):
@@ -96,7 +97,7 @@ def extract_filter_keys(request):
           }
     for key in settings.OUTTER_KEYS:
         try:
-            val = request.GET.__getitem__(key)
+            val = dict(request.GET)[key]
             dic['filter_keys'].append({
                 key: val,
                 })
@@ -119,16 +120,16 @@ def extract_aggregate_key(request):
     aggs_type = []
     aggs_field = []
     try:
-        aggs_type = request.GET['agg_type']
-        aggs_field = request.GET['agg_field']
+        aggs_type = dict(request.GET)['agg_type']
+        aggs_field = dict(request.GET)['agg_field']
     except KeyError as ve:
         pass
     if len(aggs_type) != len(aggs_field):
         raise IndexError
     for key1, key2 in zip(aggs_type, aggs_field):
-        if key2 in settings.AGGREGATE_FUNCS:
+        if key1 in settings.AGGREGATE_FUNCS:
             dic['agg_keys'].append({
-                key1: key2
+                key2: key1
             })
     return dic
 
@@ -166,9 +167,26 @@ def make_request_body(request, data):
 
     return final_dic
 
+def append_get_params(url, request):
+    get_params = dict(request.GET)
+    utils.ilog(LOG_CLASS, "Got following params for get: {!s}".format(get_params), mode="DEBUG")
+    url+="?"
+    param_str_list = []
+    for key in list(get_params.keys()):
+        val = get_params[key]
+        if isinstance(val, list):
+            for v in val:
+                param_str_list.append("{!s}={!s}".format(str(key).strip(), str(v).strip()))
+        else:
+            param_str_list.append("{!s}={!s}".format(str(key).strip(), str(val).strip()))
+    param_str = "&".join(param_str_list)
+    url += param_str
+    utils.ilog(LOG_CLASS, "URL formed is: {!s}".format(url))
+    return url
+
 def append_pagination(request, resp, page_keys, total_hits):
-    cur = final_dic['paging']['from']
-    limit = final_dic['paging']['size']
+    cur = page_keys['from']
+    limit = page_keys['size']
     prev_link = int(cur) - int(limit)
     next_link = int(cur) + int(limit)
     if next_link >= total_hits:
@@ -177,20 +195,22 @@ def append_pagination(request, resp, page_keys, total_hits):
         prev_link = 0
     if cur == 0:
         prev_link = None
-    if prev_link:
+    if prev_link is not None:
+        temp = None
         temp = request
         get = dict(temp.GET)
         get['start'] = prev_link
         get['limit'] = limit
         temp.GET = get
-        resp['prev_link'] = temp.build_absolute_uri()
-    if next_link:
+        resp['prev_link'] = append_get_params(temp.path, temp)
+    if next_link is not None:
+        temp = None
         temp = request
         get = dict(temp.GET)
         get['start'] = next_link
         get['limit'] = limit
         temp.GET = get
-        resp['next_link'] = temp.build_absolute_uri()
+        resp['next_link'] = append_get_params(temp.path, temp)
     return resp
 
 
@@ -227,8 +247,9 @@ def handle_response(request, data):
     else:
         obj=SearchElasticSearch()
         result = obj.search_elasticsearch(data)
+        utils.ilog(LOG_CLASS, "Returned value: {!s}".format(result), mode="ERROR")
         if 'status' in result.keys():
-            res = append_pagination(request, res, data['page_keys'], result['total_hits'])
+            res = append_pagination(request, res, data['paging'], result['total_hits'])
             res = append_key_value(res, 'status code', 200)
             res = append_key_value(res, 'total hits', result['total_hits'])
             res = append_key_value(res, 'result', result['logs'])
