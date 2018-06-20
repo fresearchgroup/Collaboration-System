63f98dc29442f1875119c684d18140f6fa29339a
abhisgithub <abhijitbonik@hotmail.com>
2018-06-20 16:31:41 +0530

    make timzone aware while searching in api with dates

--- Community/viewsets.py
+++ Community/viewsets.py
@@ -2,14 +2,15 @@ from .models import Community, CommunityArticles
 from .serializers import CommunitySerializer, CommunityArticlesSerializer
 from rest_framework import generics
 import datetime, time
+from django.utils import timezone
 # Create your views here.
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(1)
-yesterday_start = datetime.datetime.combine(yesterday, datetime.time())
-yesterday_end = datetime.datetime.combine(today, datetime.time())
+yesterday_start = timezone.make_aware(datetime.datetime.combine(yesterday, datetime.time()))
+yesterday_end = timezone.make_aware(datetime.datetime.combine(today, datetime.time()))
 
 class CommunityViewSet(generics.ListAPIView):
-	queryset = Community.objects.filter(created_at__range=[yesterday_start, yesterday_end])
+	queryset = Community.objects.filter(created_at__range=[yesterday_start,yesterday_end])
 	serializer_class = CommunitySerializer
 
 
