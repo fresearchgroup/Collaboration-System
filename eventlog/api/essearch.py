683ce1bc36f77db4516939173576df5625ac9f8c
divyansh417 <divyansh9595@gmail.com>
2018-06-20 12:46:37 +0530

    aggregation body added

--- eventlog/api/essearch.py
+++ eventlog/api/essearch.py
@@ -7,7 +7,7 @@
 
 from elasticsearch import Elasticsearch
 from . import settings
-from .. import utils
+#from .. import #utils
 
 class SearchElasticSearch:
     
@@ -26,7 +26,6 @@ class SearchElasticSearch:
                   "includes": []
             }, 
             "sort" : [],
-            "aggs" : {},
             "query" : {
                "bool" : {
                    "must":[
@@ -44,7 +43,7 @@ class SearchElasticSearch:
          }
 
         must_keys = search_key_dic['request_keys'].keys()
-        utils.ilog(self.LOG_CLASS, "Entered request keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered request keys phase...", mode="DEBUG")
         for key in must_keys:
             match={"match":{}}
             if key in self.outter_keys:
@@ -53,18 +52,18 @@ class SearchElasticSearch:
                 match["match"].update({'event.'+key:search_key_dic['request_keys'][key]})
             body["query"]["bool"]["must"].append(match)
 
-        utils.ilog(self.LOG_CLASS, "Entered paging keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered paging keys phase...", mode="DEBUG")
         if "paging" in search_key_dic.keys():
             body["from"] = search_key_dic["paging"]["from"]
             body["size"] = search_key_dic["paging"]["size"]
            
-        utils.ilog(self.LOG_CLASS, "Entered fields keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered fields keys phase...", mode="DEBUG")
         if "fields" in search_key_dic.keys():
               field_list =search_key_dic["fields"]
               for field in field_list:
                    body["_source"]["includes"].append(field)
         
-        utils.ilog(self.LOG_CLASS, "Entered sorts keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered sorts keys phase...", mode="DEBUG")
         if "sorts" in search_key_dic.keys():
             for item in search_key_dic["sorts"]:
                  for key in list(item.keys()):
@@ -73,7 +72,7 @@ class SearchElasticSearch:
                     else:
                          body["sort"].append({'event.'+str(key)+'.keyword':{"order":item[key]}})
 
-        utils.ilog(self.LOG_CLASS, "Entered filters keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered filters keys phase...", mode="DEBUG")
         if "filters" in search_key_dic.keys():
             items = search_key_dic["filters"]
             for item in items:
@@ -83,46 +82,110 @@ class SearchElasticSearch:
                 else:
                      body["query"]["bool"]["filter"].append({"term":{'event.'+key:item[key]}})
         
-        utils.ilog(self.LOG_CLASS, "Entered aggregation keys phase...", mode="DEBUG")
-        if "agg_keys" in search_key_dic.keys():
-            items = search_key_dic["agg_keys"]
-            for item in items:
-                utils.ilog(self.LOG_CLASS, "Inserting key: {!s}".format(item), mode="DEBUG")
-                key = list(item.keys())[0]
-                main_dic={}
-                if key in self.outter_keys:
-                    dic = {"field":str(key)+'.keyword'}
-                    main_dic.update({item[key]:dic})
-                else:
-                    dic = {"field":'event.'+key+'.keyword'}
-                    main_dic.update({item[key]:dic})
-                body["aggs"].update({key+'_agg':main_dic})
 
-        utils.ilog(self.LOG_CLASS, "Entered time range keys phase...", mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "Entered time range keys phase...", mode="DEBUG")
         if "time_range" in search_key_dic.keys():        
             body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"gte":search_key_dic['time_range']['after']})
             body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"lte":search_key_dic['time_range']['before']})
         
-        utils.ilog(self.LOG_CLASS, "body format: {!s}".format(body), mode="DEBUG")
+        #utils.ilog(self.LOG_CLASS, "body format: {!s}".format(body), mode="DEBUG")
 
-        return body    
+        return body
+
+    def build_search_body_agg(self,search_key_dic):
+
+        body = {
+             "size" : 0,
+             "aggs" : {},
+             "query" : {
+                "bool":{
+                    "must":[
+                      {
+                       "range" : {
+                           "time-stamp.keyword":{
+                            }
+                        }
+                      },
+                    ],
+                    "filter" : [
+                   ]
+                }
+             }
+        }
+
+        #utils.ilog(self.LOG_CLASS, "Entered time range keys phase...", mode="DEBUG")
+        if "time_range" in search_key_dic.keys():        
+            body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"gte":search_key_dic['time_range']['after']})
+            body["query"]["bool"]["must"][0]["range"]["time-stamp.keyword"].update({"lte":search_key_dic['time_range']['before']})
+        
+
+        if "paging" in search_key_dic.keys():
+            size = search_key_dic["paging"]["size"]
+        else:
+            size = 10
+
+        #utils.ilog(self.LOG_CLASS, "Entered aggregation keys phase...", mode="DEBUG")
+        if "agg_keys" in search_key_dic.keys():
+            items = search_key_dic["agg_keys"]
+            for item in items:
+                #utils.ilog(self.LOG_CLASS, "Inserting key: {!s}".format(item), mode="DEBUG")
+                key = list(item.keys())[0]
+                main_dic={}
+                if key in self.outter_keys:
+                    dic = {
+                           "field":str(key)+'.keyword'
+                          }
+                    if item[key] == 'terms':
+                        dic.update({"size":size})
+                    main_dic.update({item[key]:dic})
+                else:
+                    dic = {  
+                             "field":'event.'+key+'.keyword'
+                          }
+                    if item[key] == 'terms':
+                        dic.update({"size":size})
+                    main_dic.update({item[key]:dic})
+                body["aggs"].update({str(key)+'_agg':main_dic})
+
+        #utils.ilog(self.LOG_CLASS, "Entered filters keys phase...", mode="DEBUG")
+        if "filters" in search_key_dic.keys():
+            items = search_key_dic["filters"]
+            for item in items:
+                key = list(item.keys())[0]
+                if key in self.outter_keys:
+                     body["query"]["bool"]["filter"].append({"term":item})
+                else:
+                     body["query"]["bool"]["filter"].append({"term":{'event.'+key:item[key]}})
+
+        return body
+
                 
     
     def search_elasticsearch(self,search_key_dic):
-            response = {}  
-            utils.ilog(self.LOG_CLASS, "Running search key", mode="DEBUG")
             try:
-                body = self.build_search_body(search_key_dic)     
-                res =  self.es.search(index=self.index,body=body)
-                total_hits = res['hits']['total']
-                response = {}
-                status = 200
-                logs = []
-                for hit in res['hits']['hits']:
-                        logs.append(hit['_source'])
-                response.update({'status':status})
-                response.update({'total_hits': total_hits})
-                response.update({'logs':logs})
+                if "agg_keys" in search_key_dic.keys():
+                    body = self.build_search_body_agg(search_key_dic)
+                    res =  self.es.search(index=self.index,body=body)
+                    response = {}
+                    status = 200
+                    logs = []
+                    key = list(res["aggregations"].keys())[0]
+                    for item in res["aggregation"][key]["buckets"]:
+                        logs.append(item)
+                    response.update({status:status})
+                    response.update({'logs':logs})
+                else:
+                    body = self.build_search_body(search_key_dic)
+                    res =  self.es.search(index=self.index,body=body)
+                    total_hits = res['hits']['total']
+                    response = {}
+                    status = 200
+                    logs = []
+                    for hit in res['hits']['hits']:
+                           logs.append(hit['_source'])
+                    response.update({'status':status})
+                    response.update({'total_hits': total_hits})
+                    response.update({'logs':logs})
             except Exception as e:
                 response.update({'error': e.args})
             return response
