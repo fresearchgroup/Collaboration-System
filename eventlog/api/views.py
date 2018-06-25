baab3090d28d0f8c922837853a16138d1e99788d
Kartik Verma <kvs1297@gmail.com>
2018-06-25 23:00:16 +0530

    added authentication to eventlog api

--- eventlog/api/views.py
+++ eventlog/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
@@ -12,19 +13,22 @@ from . import helpers
 def id_match(name):
     return name+"-id"
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ) )
 def get_user_id(request, id):
     data = {'user-id': id}
     res, status_code = helpers.handle_response(request, data)
     return Response(res, status = status_code)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ) )
 def get_event(request, param1, param2):
     data = {'event_name': ".".join(["event", param1, param2])} 
     res, status_code = helpers.handle_response(request, data)
     return Response(res, status = status_code)
     
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ) )
 def get_event_id(request, param1, param2, eid):
     id_name=id_match(param1)
     data = {
@@ -34,7 +38,8 @@ def get_event_id(request, param1, param2, eid):
     res, status_code = helpers.handle_response(request, data)
     return Response(res, status = status_code)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ) )
 def get_user_id_event(request, id, param1, param2):
     data = {
                 'user-id': id,
@@ -43,7 +48,8 @@ def get_user_id_event(request, id, param1, param2):
     res, status_code = helpers.handle_response(request, data)
     return Response(res, status = status_code)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ) )
 def get_user_id_event_id(request, id, param1, param2, eid):
     id_name=id_match(param1)
     data = {
