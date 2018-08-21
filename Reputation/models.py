8db5a6347e6dfd1e7d0964e722a52411d490ece9
abhisgithub <abhijitbonik@hotmail.com>
2018-08-21 14:21:47 +0530

    community reputation model added

--- Reputation/models.py
+++ Reputation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Community.models import Community
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CommunityReputaion(models.Model): 
+	community = models.ForeignKey(Community ,on_delete = models.CASCADE)
+	user = models.ForeignKey(User , on_delete = models.CASCADE)
+	score = models.IntegerField()
