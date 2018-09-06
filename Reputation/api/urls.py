831bb2fbab1ccb6079b08c856aa9c10c3ba3c70c
abhisgithub <abhijitbonik@hotmail.com>
2018-09-06 17:01:33 +0530

    apis updated to function based

--- Reputation/api/urls.py
+++ Reputation/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import FetchCommunityReputation
+from .views import FetchCommunityReputation, ArticlePublishScore
 
 urlpatterns = [
 
     url(r'^get_reputation/(?P<pk>\d+)/$', FetchCommunityReputation.as_view(), name='get_reputation'),
+    url(r'^article_publish_score/$', ArticlePublishScore, name='article_publish_score'),
 ]
--- Reputation/api/views.py
+++ Reputation/api/views.py
@@ -24,27 +24,25 @@ class FetchCommunityReputation(generics.RetrieveUpdateDestroyAPIView):
                 return CommunityReputaion.objects.create(community=community, user = self.request.user)
 
 
-class ArticlePublishScore(generics.CreateAPIView):
-    lookup_field = 'pk'
-    serializer_class = CommunityReputaionSerializer
-    permission_classes = (IsAuthenticated,)
+@api_view(['POST'])
+def ArticlePublishScore(request):
+    articleid = request.data['pk']
+    article = ArticleScoreLog.objects.get(article=articleid)
 
-    def perform_create(self, serializer):
-        articleid = self.kwargs['pk']
-        artcile = ArticleScoreLog.objects.get(article=articleid)
-
-        if not artcile.publish and article.article.state.name=='publish':
-            res = ResourceScore.objects.get(resource_type='resource')
-            if CommunityArticles.objects.filter(article=artcile.article).exists():
-                comm_article= CommunityArticles.objects.get(article=artcile.article)
-                repu = CommunityReputaion.objects.get(community=comm_article.community, user = article.article.created_by)
-                repu.score += res.publish_value
-                repu.save()
-                return repu
-            else:
-                group_article = GroupArticles.objects.get(artcile= article.article)
-                comm_group = CommunityGroups.objects.get(group=group_article.group)
-                repu = CommunityReputaion.objects.get(community=comm_group.community, user = article.article.created_by)
-                repu.score += res.publish_value
-                repu.save()
-                return repu
+    if not article.publish and article.article.state.name=='publish':
+        res = ResourceScore.objects.get(resource_type='resource')
+        if CommunityArticles.objects.filter(article=article.article).exists():
+            comm_article= CommunityArticles.objects.get(article=article.article)
+            repu = CommunityReputaion.objects.get(community=comm_article.community, user = article.article.created_by)
+            repu.score += res.publish_value
+            repu.save()
+            repu = CommunityReputaionSerializer(repu, many=False)
+            return Response(repu.data)
+        else:
+            group_article = GroupArticles.objects.get(artcile= article.article)
+            comm_group = CommunityGroups.objects.get(group=group_article.group)
+            repu = CommunityReputaion.objects.get(community=comm_group.community, user = article.article.created_by)
+            repu.score += res.publish_value
+            repu.save()
+            repu = CommunityReputaionSerializer(repu, many=False)
+            return Response(repu.data)
