644f54f78e028a32893a3af966b71fd264d0d429
abhisgithub <abhijitbonik@hotmail.com>
2018-09-06 19:39:44 +0530

    create ether pad id for group

--- etherpad/views.py
+++ etherpad/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from py_etherpad import EtherpadLiteClient
 from django.conf import settings
-from .models import EtherCommunity
+from .models import EtherCommunity, EtherGroup
 
 def create_community_ether(community):
     epclient = EtherpadLiteClient(settings.APIKEY, settings.APIURL)
@@ -9,3 +9,10 @@ def create_community_ether(community):
     EtherCommunity.objects.create(community=community, community_ether_id=result['groupID'])
     return
 
+
+def create_group_ether(community_id, group):
+    epclient = EtherpadLiteClient(settings.APIKEY, settings.APIURL)
+    group_id = str(community_id) + str(group)
+    result =  epclient.createGroupIfNotExistsFor(group_id)
+    EtherGroup.objects.create(group=group, group_ether_id=result['groupID'])
+    return
