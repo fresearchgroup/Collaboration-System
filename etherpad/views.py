9ecdce5f741dd211b661dd26f30ec0447da64b72
abhisgithub <abhijitbonik@hotmail.com>
2018-09-06 20:15:49 +0530

    create ether pad for article in community

--- etherpad/views.py
+++ etherpad/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from py_etherpad import EtherpadLiteClient
 from django.conf import settings
-from .models import EtherCommunity, EtherGroup
+from .models import EtherCommunity, EtherGroup, EtherArticle, EtherUser
 
 def create_community_ether(community):
     epclient = EtherpadLiteClient(settings.APIKEY, settings.APIURL)
@@ -16,3 +16,10 @@ def create_group_ether(community_id, group):
     result =  epclient.createGroupIfNotExistsFor(group_id)
     EtherGroup.objects.create(group=group, group_ether_id=result['groupID'])
     return
+
+def create_article_ether_community(community_id, article):
+    epclient = EtherpadLiteClient(settings.APIKEY, settings.APIURL)
+    community = EtherCommunity.objects.get(community=community_id)
+    result =  epclient.createGroupPad(community.community_ether_id, article.id)
+    EtherArticle.objects.create(article=article, article_ether_id=result['padID'])
+    
