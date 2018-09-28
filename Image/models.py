ccbb20c44096c9c1bda2d9182766b358319bc4ae
abhisgithub <abhijitbonik@hotmail.com>
2018-09-28 17:14:50 +0530

    create image and view image added

--- Image/models.py
+++ Image/models.py
@@ -5,16 +5,16 @@ import os, uuid
 from django.conf import settings
 
 def get_file_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join('article', filename)
+	ext = filename.split('.')[-1]
+	filename = "%s.%s" % (uuid.uuid4(), ext)
+	return os.path.join('images', filename)
 
-class Image(models.Model):
+class Images(models.Model):
 	title = models.CharField(max_length=100)
 	image = models.ImageField(null=True,upload_to=get_file_path)
 	created_at = models.DateTimeField(auto_now_add=True)
-	created_by = models.ForeignKey(User,null=True,related_name='article_author')
+	created_by = models.ForeignKey(User,null=True,related_name='image_author')
 	published_on=models.DateTimeField(null=True)
-	published_by=models.ForeignKey(User,null=True,related_name='article_publisher')
+	published_by=models.ForeignKey(User,null=True,related_name='image_publisher')
 	views = models.PositiveIntegerField(default=0)
-	state = models.ForeignKey(States, null=True,related_name='articleworkflow')
+	state = models.ForeignKey(States, null=True,related_name='imageworkflow')
--- Image/views.py
+++ Image/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Images
+from workflow.models import States
+from Community.models import CommunityImages, CommunityMembership
+from workflow.views import canEditResourceCommunity
 
-# Create your views here.
+def create_image(request):
+	"""
+	create a new image. This function will be called for creating an
+	image in community or group.
+	"""
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			state = States.objects.get(name='draft')
+			title = request.POST['title']
+			image = request.FILES['image']
+			article = Images.objects.create(
+				title = title,
+				image = image,
+				created_by = request.user,
+				state = state
+				)
+			return article
+	else:
+		return redirect('login')
+
+def image_view(request, pk):
+	try:
+		image = CommunityImages.objects.get(image=pk)
+		if image.image.state == States.objects.get(name='draft') and image.image.created_by != request.user:
+			return redirect('home')
+		canEdit = ""
+		if CommunityMembership.objects.filter(user=request.user.id, community=image.community).exists():
+			membership = CommunityMembership.objects.get(user=request.user.id, community=image.community)
+			canEdit = canEditResourceCommunity(image.image.state.name, membership.role.name, image.image, request)
+	except CommunityImages.DoesNotExist:
+		raise Http404
+	return render(request, 'view_image.html', {'image':image, 'canEdit':canEdit})
+ 
