bc55c094e14fb43daaa5a918267b0c8c4cf40553
abhisgithub <abhijitbonik@hotmail.com>
2018-09-28 12:20:38 +0530

    image models added

--- Image/models.py
+++ Image/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from workflow.models import States
+import os, uuid
+from django.conf import settings
 
-# Create your models here.
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('article', filename)
+
+class Image(models.Model):
+	title = models.CharField(max_length=100)
+	image = models.ImageField(null=True,upload_to=get_file_path)
+	created_at = models.DateTimeField(auto_now_add=True)
+	created_by = models.ForeignKey(User,null=True,related_name='article_author')
+	published_on=models.DateTimeField(null=True)
+	published_by=models.ForeignKey(User,null=True,related_name='article_publisher')
+	views = models.PositiveIntegerField(default=0)
+	state = models.ForeignKey(States, null=True,related_name='articleworkflow')
