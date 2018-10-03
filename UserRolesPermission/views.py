50951d493aff254efc75e83aa2eb6c52c9f7a0be
firuza <firya87@gmail.com>
2018-10-03 14:39:22 +0530

    displaying draft images on user dashboard

--- UserRolesPermission/views.py
+++ UserRolesPermission/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
-from Community.models import CommunityMembership, CommunityArticles, CommunityGroups, RequestCommunityCreation, CommunityCourses
+from Community.models import CommunityMembership, CommunityArticles, CommunityGroups, RequestCommunityCreation, CommunityCourses, CommunityImages
 from BasicArticle.models import Articles
 from .forms import SignUpForm
 from .roles import Author
@@ -92,6 +92,7 @@ def user_dashboard(request):
         commarticles = CommunityArticles.objects.filter(user=request.user)
         grparticles = GroupArticles.objects.filter(user=request.user)
         commcourses = CommunityCourses.objects.filter(user=request.user)
+        commimages = CommunityImages.objects.filter(user=request.user)
 
         for cart in commarticles:
             cart.type = 'article'
@@ -105,7 +106,11 @@ def user_dashboard(request):
             ccourse.type = 'course'
             ccourse.belongsto = 'community'
 
-        lstContent = list(commarticles) + list(grparticles) + list(commcourses)
+        for cimages in commimages:
+            cimages.type = 'Image'
+            cimages.belongsto = 'community'
+
+        lstContent = list(commarticles) + list(grparticles) +  list(commimages) + list(commcourses)
 
         pendingcommunities=RequestCommunityCreation.objects.filter(status='Request', requestedby=request.user)
         grpinvitations = GroupInvitations.objects.filter(status='Invited', user=request.user)
