7a554f124d8b4d5c955f5e4f369ad33e2ce8a29a
firuza <firya87@gmail.com>
2018-10-04 11:59:55 +0530

    Added Media model

--- Media/models.py
+++ Media/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from workflow.models import States
+import os, uuid
+from django.conf import settings
 
-# Create your models here.
+def get_file_path(instance, filename):
+	ext = filename.split('.')[-1]
+	filename = "%s.%s" % (uuid.uuid4(), ext)
+	if ext == 'mp3' or ext == 'wav' or ext == 'ogg' :
+		return os.path.join('audio', filename)
+	if ext == 'jpg' or ext == 'jpeg' or ext == 'png':
+		return os.path.join('images', filename)
+	if ext == 'vob' or ext == 'webm' or ext == 'avi' or ext == 'mp4':
+		return os.path.join('video', filename)
+
+class Media(models.Model):
+	media_types = (
+		('IMAGE', 'Image'),
+        ('AUDIO', 'Audio'),
+        ('VIDEO', 'Video'),
+    )
+	mediatype = models.CharField(choices=media_types, default='IMAGE')	
+	title = models.CharField(max_length=100)
+	mediafile = models.FileField(null=True,upload_to=get_file_path)
+	created_at = models.DateTimeField(auto_now_add=True)
+	created_by = models.ForeignKey(User,null=True,related_name='image_author')
+	published_on=models.DateTimeField(null=True)
+	published_by=models.ForeignKey(User,null=True,related_name='image_publisher')
+	views = models.PositiveIntegerField(default=0)
+	state = models.ForeignKey(States, null=True,related_name='imageworkflow')
