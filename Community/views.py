306e2af53dbefdf2bba8a93e2c1e83bd59bb8312
firuza <firya87@gmail.com>
2018-10-04 13:17:49 +0530

    Create and view image media files

--- Community/views.py
+++ Community/views.py
@@ -5,7 +5,7 @@ from BasicArticle.views import create_article, view_article, getHTML
 from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from BasicArticle.models import Articles
-from .models import Community, CommunityMembership, CommunityArticles, RequestCommunityCreation, CommunityGroups, CommunityCourses, CommunityImages
+from .models import Community, CommunityMembership, CommunityArticles, RequestCommunityCreation, CommunityGroups, CommunityCourses, CommunityImages, CommunityMedia
 from rest_framework import viewsets
 from .models import CommunityGroups
 from Group.views import create_group
@@ -32,6 +32,7 @@ import json
 import requests
 from etherpad.views import create_community_ether, create_article_ether_community, create_session_community
 from Image.views import create_image
+from Media.views import create_media
 
 def display_communities(request):
 	if request.method == 'POST':
@@ -675,18 +676,18 @@ def create_wiki_for_community(community):
 
 	cursor.execute('''SET FOREIGN_KEY_CHECKS=1''')
 
-def community_image_create(request):
+def community_media_create(request):
 	if request.user.is_authenticated:
 		if request.method == 'POST':
 			status = request.POST['status']
 			cid = request.POST['cid']
 			community = Community.objects.get(pk=cid)
 			if status=='1':
-				image = create_image(request)
-				CommunityImages.objects.create(image_resource=image, user=request.user, community=community)
-				return redirect('image_view', image.pk)
+				media = create_media(request)
+				CommunityMedia.objects.create(media=media, user=request.user, community=community)
+				return redirect('media_view', media.pk)
 			else:
-				return render(request, 'new_image.html', {'community':community, 'status':1})
+				return render(request, 'new_media.html', {'community':community, 'status':1})
 		else:
 			return redirect('home')
 	else:
--- Media/views.py
+++ Media/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Media
+from workflow.models import States
+from Community.models import CommunityMedia, CommunityMembership, Community
+from workflow.views import canEditResourceCommunity
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def create_media(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			state = States.objects.get(name='draft')
+			title = request.POST['title']
+			mediatype = request.POST['mediatype']
+			mediafile = request.FILES['mediafile']
+			media_resource = Media.objects.create(
+				title = title,
+				mediatype = mediatype,
+				mediafile = mediafile,
+				created_by = request.user,
+				state = state
+				)
+			return media_resource
+	else:
+		return redirect('login')
+
+def media_view(request, pk):
+	try:
+		cmedia = CommunityMedia.objects.get(media=pk)
+		if cmedia.media.state == States.objects.get(name='draft') and cmedia.media.created_by != request.user:
+			return redirect('home')
+		canEdit = ""
+		if CommunityMembership.objects.filter(user=request.user.id, community=cmedia.community).exists():
+			membership = CommunityMembership.objects.get(user=request.user.id, community=cmedia.community)
+			canEdit = canEditResourceCommunity(cmedia.media.state.name, membership.role.name, cmedia.media, request)
+	except CommunityImages.DoesNotExist:
+		raise Http404
+	return render(request, 'view_media.html', {'cmedia':cmedia, 'canEdit':canEdit})
