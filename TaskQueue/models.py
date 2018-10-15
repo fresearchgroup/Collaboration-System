1c490fd98210642b8d30b9038db0aebd2455b463
abhisgithub <abhijitbonik@hotmail.com>
2018-10-15 19:42:22 +0530

    models for task

--- TaskQueue/models.py
+++ TaskQueue/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    tfile = models.FileField(upload_to='task/')
+    status = models.BooleanField(default=False)
+    
