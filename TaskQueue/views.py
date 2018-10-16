2ce9900767a212c2f64ee747d3cf4d61c6aa20ce
abhisgithub <abhijitbonik@hotmail.com>
2018-10-16 12:23:53 +0530

    run celery

--- TaskQueue/views.py
+++ TaskQueue/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from .tasks import createbulkcommunity
+from django.contrib import messages
 # Create your views here.
 
 def upload_task(request):
     if request.user.is_superuser:
         if request.method == 'POST':
             name = request.POST['name']
-            task = request.POST['file']
-            Task.objects.create(name=name, task=task)
+            task = request.FILES['taskfile']
+            Task.objects.create(name=name, tfile=task)
             return redirect('upload_task')
         else:
             task = Task.objects.all()
-            return render('tasks.html',{'task':task})
+            return render(request, 'tasks.html',{'task':task})
+    else:
+        return redirect('home')
+
+def run_task(request):
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            taskid = request.POST['task']
+            task = Task.objects.get(pk=taskid)
+            createbulkcommunity.delay(task)
+            messages.success(self.request, 'We are generating your random users! Wait a moment and refresh this page.')
+            return redirect('upload_task')
+        else:
+            return redirect('upload_task')
     else:
         return redirect('home')
