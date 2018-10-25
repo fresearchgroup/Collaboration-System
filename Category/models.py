bf2ed562fb195711766364cafab23cbbc49d7216
firuza <firya87@gmail.com>
2018-10-25 19:43:01 +0530

    Added images to category model

--- Category/models.py
+++ Category/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from Group.models import Group
 from Community.models import Community
+import os, uuid
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('category', filename)
 
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 	desc = models.TextField()
+	image = models.ImageField(null=True, upload_to=get_file_path)
 
 	def __str__(self):
 		return self.name
