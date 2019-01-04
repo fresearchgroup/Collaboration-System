0840d5ddeaab7f8426e6c6d999e42ae406a8ca7d
firuza <firya87@gmail.com>
2019-01-04 12:02:12 +0530

    removed groups from user_dashboard

--- UserRolesPermission/views.py
+++ UserRolesPermission/views.py
@@ -87,21 +87,14 @@ def user_dashboard(request):
             user_profile = "No Image available"
 
         mycommunities = CommunityMembership.objects.filter(user=request.user).order_by('community__name')
-        mygroups = GroupMembership.objects.filter(user=request.user).order_by('group__name')
 
         commarticles = CommunityArticles.objects.filter(user=request.user)
-        grparticles = GroupArticles.objects.filter(user=request.user)
         commcourses = CommunityCourses.objects.filter(user=request.user)
         commmedia = CommunityMedia.objects.filter(user=request.user)
-        grpmedia = GroupMedia.objects.filter(user=request.user)
 
         for cart in commarticles:
             cart.type = 'article'
             cart.belongsto = 'community'
-
-        for gart in grparticles:
-            gart.type = 'article'
-            gart.belongsto = 'group'
 
         for ccourse in commcourses:
             ccourse.type = 'course'
@@ -111,14 +104,9 @@ def user_dashboard(request):
             cmedia.type = 'Media'
             cmedia.belongsto = 'community'
 
-        for gmedia in grpmedia:
-            gmedia.type = 'Media'
-            gmedia.belongsto = 'group'
-
-        lstContent = list(commarticles) + list(grparticles) +  list(commmedia) + list(grpmedia) + list(commcourses)
+        lstContent = list(commarticles) + list(commmedia) + list(commcourses)
 
         pendingcommunities=RequestCommunityCreation.objects.filter(status='Request', requestedby=request.user)
-        grpinvitations = GroupInvitations.objects.filter(status='Invited', user=request.user)
 
         ap = User.objects.raw(
         'Select 1 id, Year, Sum(JAN) JAN,sum(FEB) FEB,sum(MAR) MAR,sum(APR) APR,sum(MAY) MAY,sum(JUN) JUN,sum(JUL) JUL,sum(AUG) AUG,sum(SEP) SEP,sum(OCT) OCT,sum(NOV) NOV,sum(DECEM) DECE, Concat(Sum(JAN) , "," , sum(FEB) , "," ,sum(MAR),",",sum(APR),",",sum(MAY),",",sum(JUN),",",sum(JUL),",",sum(AUG),",", sum(SEP) ,",",sum(OCT),",",sum(NOV),",",sum(DECEM)) Data,state_id State from (Select Year, Case when Month=1 Then 1 else 0 END JAN, Case when Month=2 Then 1 else 0 END FEB, Case when Month=3 Then 1 else 0 END MAR, Case when Month=4 Then 1 else 0 END APR, Case when Month=5 Then 1 else 0 END MAY, Case when Month=6 Then 1 else 0 END JUN, Case when Month=7 Then 1 else 0 END JUL, Case when Month=8 Then 1 else 0 END AUG, Case when Month=9 Then 1 else 0 END SEP, Case when Month=10 Then 1 else 0 END OCT, Case when Month=11 Then 1 else 0 END NOV, Case when Month=12 Then 1 else 0 END DECEM, state_id from  (select  Month(created_at) Month ,Year(created_at) Year ,state_id from BasicArticle_articles where id in (select article_id from Community_communityarticles where user_id=%s) or id in (select article_id from Group_grouparticles where user_id=%s)) T ) P group by Year,state_id having Year=%s;',
@@ -142,7 +130,7 @@ def user_dashboard(request):
         for a in articlescontributed:
             total = total + ',' + a
         total=total[1:]
-        return render(request, 'userdashboard.html', {'mycommunities':mycommunities, 'mygroups':mygroups, 'commarticles':commarticles, 'grparticles':grparticles, 'pendingcommunities':pendingcommunities,'articlescontributed':list(articlescontributed),'articlespublished':articlespublished, 'total':total, 'user_profile':user_profile, 'grpinvitations':grpinvitations, 'lstContent':lstContent})
+        return render(request, 'userdashboard.html', {'mycommunities':mycommunities, 'commarticles':commarticles, 'pendingcommunities':pendingcommunities,'articlescontributed':list(articlescontributed),'articlespublished':articlespublished, 'total':total, 'user_profile':user_profile, 'lstContent':lstContent})
     else:
         return redirect('login')
 
