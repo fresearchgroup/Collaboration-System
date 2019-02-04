035276ee9f5156aa2b25426a34c476c1f0fb30bb
shon <abhijitbonik@hotmail.com>
2019-02-04 20:02:37 +0530

    request community form

--- Community/forms.py
+++ Community/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Community
+from .models import Community, RequestCommunityCreation
 
 class CommunityCreateForm(forms.ModelForm):
 	class Meta:
@@ -15,3 +15,17 @@ class CommunityCreateForm(forms.ModelForm):
 		self.fields['category'].widget.attrs.update({'class': 'form-control'})
 		self.fields['tag_line'].widget.attrs.update({'class': 'form-control'})
 		self.fields['created_by'].widget.attrs.update({'class': 'form-control'})
+
+
+class RequestCommunityCreateForm(forms.ModelForm):
+	class Meta:
+		model = RequestCommunityCreation
+		fields = ['name', 'desc', 'category', 'tag_line', 'purpose']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs.update({'class': 'form-control', 'ng-model':'name', 'ng-pattern':'/^[a-z A-Z ()]*$/'})
+		self.fields['desc'].widget.attrs.update({'class': 'form-control'})
+		self.fields['category'].widget.attrs.update({'class': 'form-control'})
+		self.fields['tag_line'].widget.attrs.update({'class': 'form-control', 'ng-model':'tag_line', 'ng-pattern': "/^[a-z A-Z0-9 !&()':-]*$/"})
+		self.fields['purpose'].widget.attrs.update({'class': 'form-control', 'ng-model':'purpose', 'ng-pattern': "/^[a-z A-Z0-9 !&()':-]*$/"})
