9ff8f6125ac4787d8364fe09fb9afc1f019bb26f
shon <abhijitbonik@hotmail.com>
2019-02-04 21:54:43 +0530

    form for community updated

--- Community/forms.py
+++ Community/forms.py
@@ -29,3 +29,18 @@ class RequestCommunityCreateForm(forms.ModelForm):
 		self.fields['category'].widget.attrs.update({'class': 'form-control'})
 		self.fields['tag_line'].widget.attrs.update({'class': 'form-control', 'ng-model':'tag_line', 'ng-pattern': "/^[a-z A-Z0-9 !&()':-]*$/"})
 		self.fields['purpose'].widget.attrs.update({'class': 'form-control', 'ng-model':'purpose', 'ng-pattern': "/^[a-z A-Z0-9 !&()':-]*$/"})
+
+class CommunityUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Community
+		fields = ['name', 'desc', 'image', 'category', 'tag_line']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs.update({'class': 'form-control'})
+		self.fields['name'].widget.attrs['readonly'] = True
+		self.fields['desc'].widget.attrs.update({'class': 'form-control'})
+		self.fields['image'].widget.attrs.update({'class': 'file', 'data-allowed-file-extensions':'["jpeg", "jpg","png"]', 'data-show-upload':'false', 'data-show-preview':'false', 'data-msg-placeholder':'Select article image for upload...'})
+		self.fields['image'].required = False
+		self.fields['category'].widget.attrs.update({'class': 'form-control'})
+		self.fields['tag_line'].widget.attrs.update({'class': 'form-control'})
