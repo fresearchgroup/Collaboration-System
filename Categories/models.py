77489f8ebb30308b3119b2b4067a694d7a51be3a
shon <abhijitbonik@hotmail.com>
2019-02-05 17:17:37 +0530

    categories app added

---
+++ Categories/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+# Create your models here.
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('community', filename)
+    
+class Category(MPTTModel):
+
+        name = models.CharField(max_length=100)
+        image = models.ImageField(null=True, upload_to=get_file_path)
+        created_at = models.DateTimeField(null=True, auto_now_add=True)
+        parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+
+        def __str__(self):
+            return self.name
