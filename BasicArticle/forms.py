318f7be783d8aa476d7e6518dda19e30e9a775ff
shon <abhijitbonik@hotmail.com>
2019-02-06 21:29:30 +0530

    form for create article

--- BasicArticle/forms.py
+++ BasicArticle/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Articles
 from workflow.views import getStatesCommunity
 from workflow.models import States
+from django.conf import settings
 
 class NewArticleForm(forms.Form):
 	title  = forms.CharField()
@@ -21,3 +22,19 @@ class ArticleUpdateForm(forms.ModelForm):
 		self.fields['state'].widget.attrs.update({'class': 'form-control'})
 		states = getStatesCommunity(self.instance.state.name)
 		self.fields['state'].queryset = States.objects.filter(name__in=states)
+
+class ArticleCreateForm(forms.ModelForm):
+	class Meta:
+		model = Articles
+		if settings.REALTIME_EDITOR:
+			fields = ['title', 'image']
+		else:
+			fields = ['title', 'body', 'image']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['title'].widget.attrs.update({'class': 'form-control'})
+		self.fields['image'].widget.attrs.update({'class': 'file', 'data-allowed-file-extensions':'["jpeg", "jpg","png"]', 'data-show-upload':'false', 'data-show-preview':'false', 'data-msg-placeholder':'Select article image for upload...'})
+		self.fields['image'].required = False
+		if not settings.REALTIME_EDITOR:
+			self.fields['body'].widget.attrs.update({'class': 'form-control'})
