a76aaafffb1d5027e0cb9fe4e092f559371aa9ec
firuza <firya87@gmail.com>
2019-02-13 17:49:58 +0530

    Draggable mptt for category

--- Categories/admin.py
+++ Categories/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Category
+from mptt.admin import DraggableMPTTAdmin
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(
+    Category,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),)
