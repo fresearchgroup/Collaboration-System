c61cdfb11e85cc5b227b82f74e99678eae79f0fe
Rohit Prasad <rohitrp3@gmail.com>
2019-02-13 13:29:45 +0530

    Add signal to create CommunityReputation object when user joins a community

--- Reputation/api/views.py
+++ Reputation/api/views.py
@@ -18,48 +18,6 @@ import json
 from badges.models import BadgeToUser
 from .serializers import BadgeToUserSerializer
 
-class FetchCommunityReputation(generics.RetrieveUpdateDestroyAPIView):
-    lookup_field = 'pk'
-    serializer_class = CommunityReputaionSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def get_object(self):
-        cid = self.kwargs['pk']
-        community = Community.objects.get(id=cid)
-        if CommunityMembership.objects.filter(community=community, user=self.request.user).exists():
-            if CommunityReputaion.objects.filter(community=community, user=self.request.user).exists():
-                return CommunityReputaion.objects.get(community=community, user=self.request.user)
-            else:
-                return CommunityReputaion.objects.create(community=community, user=self.request.user)
-
-
-@api_view(['POST'])
-def ArticlePublishScore(request):
-    articleid = request.data['pk']
-    article = ArticleScoreLog.objects.get(article=articleid)
-
-    if not article.publish and article.article.state.name == 'publish':
-        res = ResourceScore.objects.get(resource_type='resource')
-        if CommunityArticles.objects.filter(article=article.article).exists():
-            comm_article = CommunityArticles.objects.get(
-                article=article.article)
-            repu = CommunityReputaion.objects.get(
-                community=comm_article.community, user=article.article.created_by)
-            repu.score += res.publish_value
-            repu.save()
-            repu = CommunityReputaionSerializer(repu, many=False)
-            return Response(repu.data)
-        else:
-            group_article = GroupArticles.objects.get(artcile=article.article)
-            comm_group = CommunityGroups.objects.get(group=group_article.group)
-            repu = CommunityReputaion.objects.get(
-                community=comm_group.community, user=article.article.created_by)
-            repu.score += res.publish_value
-            repu.save()
-            repu = CommunityReputaionSerializer(repu, many=False)
-            return Response(repu.data)
-
-
 class ReputationStats(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
 
@@ -220,6 +178,8 @@ class FlagReasons(generics.ListCreateAPIView):
     serializer_class = FlagReasonSerializer
 
 class ResourceReports(APIView):
+    permission_classes = (IsAuthenticated,)
+    
     def get_models(self, request):
         resource_type = self.request.query_params.get('resource_type')
 
@@ -245,6 +205,8 @@ class ResourceReports(APIView):
         return Response(response)
 
 class ReputationScore(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         reputation_score = []
 
@@ -260,5 +222,4 @@ class ReputationScore(APIView):
                 'badges': BadgeToUserSerializer(BadgeToUser.objects.filter(user=request.user, community=repu.community), many=True).data
             })
 
-        
         return Response(json.dumps(reputation_score))
--- Reputation/apps.py
+++ Reputation/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save
 
 class ReputationConfig(AppConfig):
     name = 'Reputation'
+
+    def ready(self):
+        from Community.models import CommunityMembership
+        from .signals import create_community_reputation
+        post_save.connect(create_community_reputation, sender=CommunityMembership, dispatch_uid='create_community_reputation')
