372c87b063316cd8c2d08855ad92932e0748e3f9
Rohit Prasad <rohitrp3@gmail.com>
2019-03-07 02:47:28 +0530

    Add inputs for achieving publisher and author role

--- Reputation/models.py
+++ Reputation/models.py
@@ -24,8 +24,8 @@ class ResourceScore(models.Model):
 
 #score needed to achieve a certain role
 class UserScore(models.Model):
-    author = models.IntegerField(null=True)
-    publisher = models.IntegerField(null=True)
+    author = models.IntegerField(default=0)
+    publisher = models.IntegerField(default=0)
     role_score = models.CharField(max_length=20, default='role_score')
 
 class FlagReason(models.Model):
