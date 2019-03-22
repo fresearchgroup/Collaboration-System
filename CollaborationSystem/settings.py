52a1a62a1e28229312b40e7328ed691f3a640cec
shon <abhijitbonik@hotmail.com>
2019-03-22 15:17:40 +0530

    community resource create api

--- CollaborationSystem/settings.py
+++ CollaborationSystem/settings.py
@@ -284,7 +284,7 @@ REST_FRAMEWORK = {
     ),
     
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
