163ad9e269bf299f8d2dcc3abfc9f7cff446dac7
shon <abhijitbonik@hotmail.com>
2019-08-20 21:08:36 +0530

    search url update with get parameter based search

--- search/api/views.py
+++ search/api/views.py
@@ -8,17 +8,26 @@ class SearchCommunityAPI(generics.ListAPIView):
 	serializer_class = SearchCommunitySerializer
 
 	def get_queryset(self):
-		return Community.objects.filter(name__icontains=self.kwargs['query'])
+		query =self.request.query_params.get('query', None)
+		if query:
+			return Community.objects.filter(name__icontains=query)
+		return Community.objects.none()
 
 
 class SearchArticleAPI(generics.ListAPIView):
 	serializer_class = SearchArticleSerializer
 
 	def get_queryset(self):
-		return Articles.objects.filter(title__icontains=self.kwargs['query'], state__name='publish')
+		query =self.request.query_params.get('query', None)
+		if query:
+			return Articles.objects.filter(title__icontains=query, state__name='publish')
+		return Articles.objects.none()
 
 class SearchMediaAPI(generics.ListAPIView):
 	serializer_class = SearchMediaSerializer
 
 	def get_queryset(self):
-		return Media.objects.filter(title__icontains=self.kwargs['query'], state__name='publish')
+		query =self.request.query_params.get('query', None)
+		if query:
+			return Media.objects.filter(title__icontains=query, state__name='publish')
+		return Media.objects.none()
