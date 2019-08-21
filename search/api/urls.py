a2dd571383685cb350ca1cde18ba2bab6860809d
shon <abhijitbonik@hotmail.com>
2019-08-21 12:23:01 +0530

    url error resolved

--- search/api/urls.py
+++ search/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import SearchCommunityAPI, SearchArticleAPI, SearchMediaAPI
 
 urlpatterns = [
-	url(r'^community', SearchCommunityAPI.as_view(), name='community-search-api'),
-	url(r'^article', SearchArticleAPI.as_view(), name='article-search-api'),
-	url(r'^media', SearchMediaAPI.as_view(), name='media-search-api'),
+	url(r'^community$', SearchCommunityAPI.as_view(), name='community-search-api'),
+	url(r'^article$', SearchArticleAPI.as_view(), name='article-search-api'),
+	url(r'^media$', SearchMediaAPI.as_view(), name='media-search-api'),
 	]
