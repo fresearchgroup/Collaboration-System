ebfbb53388917d129f6dcd70b25ef85ccc3adf79
shon <abhijitbonik@hotmail.com>
2019-08-22 18:51:22 +0530

    elastic index schema for article

--- search/schema.py
+++ search/schema.py
@@ -21,3 +21,24 @@ class CommunityIndex(Document):
 
     def save(self, ** kwargs):
         return super(CommunityIndex, self).save(** kwargs)
+
+
+class ArticleIndex(Document):
+    article_id = Integer()
+    title = Text(analyzer='snowball', fields={'raw': Keyword()})
+    body = Text(analyzer='snowball')
+    image = Text(analyzer='snowball')
+    created_at = Date()
+    created_by = Text(analyzer='snowball', fields={'raw': Keyword()})
+    published_on = Date()
+    published_by = Text(analyzer='snowball', fields={'raw': Keyword()})
+    views = Integer()
+
+    class Index:
+        name = 'collab'
+        settings = {
+          "number_of_shards": 2,
+        }
+
+    def save(self, ** kwargs):
+        return super(ArticleIndex, self).save(** kwargs)
