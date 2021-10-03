40d17ccbc3122e65d4e3639c02869b3bc8170f43
firuza <firya87@gmail.com>
2021-10-04 01:44:00 +0530

    update backtotop

--- static/assets/corporate/scripts/back-to-top.js
+++ static/assets/corporate/scripts/back-to-top.js
@@ -9,7 +9,7 @@ var scrolltotop={
 	//startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
 	//scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
 	setting: {startline:100, scrollto: 0, scrollduration:1000, fadeduration:[500, 100]},
-	controlHTML: '<i class="fa fa-arrow-up fa-3x" style="color: #89216B"></i>', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
+	controlHTML: '<i class="fa fa-chevron-circle-up fa-3x" style="color: #89216B"></i>', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
 	controlattrs: {offsetx:10, offsety:10}, //offset of control relative to right/ bottom of window corner
 	anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
 
