2ce35d99ba032dab1e8b0b0b62a52c5ba7cca2c9
Nagesh Karmali <nags.ides@gmail.com>
2021-10-25 16:37:02 +0530

    updated fields in edit article

--- BasicArticle/forms.py
+++ BasicArticle/forms.py
@@ -13,16 +13,31 @@ class ArticleUpdateForm(forms.ModelForm):
 	class Meta:
 		model = Articles
 		if settings.REALTIME_EDITOR:
-			fields = ['title', 'image', 'state', 'tags']
+			# fields = ['title', 'image', 'state', 'tags']
+			pass
 		else:
-			fields = ['title', 'body', 'image', 'state', 'tags']
+			# fields = ['title', 'body', 'image', 'state', 'tags']
+			fields = ['introduction', 'architecture', 'rituals', 'ceremonies', 'tales', 'more_information', 'state', 'tags']
+
 
 	def __init__(self, *args, **kwargs):
 		role = kwargs.pop('role', None)
 		super().__init__(*args, **kwargs)
-		self.fields['title'].widget.attrs.update({'class': 'form-control'})
-		self.fields['image'].widget.attrs.update({'class': 'file', 'data-allowed-file-extensions':'["jpeg", "jpg","png"]', 'data-show-upload':'false', 'data-show-preview':'false', 'data-msg-placeholder':'Select article image for upload...'})
-		self.fields['image'].required = False
+		# self.fields['title'].widget.attrs.update({'class': 'form-control'})
+		self.fields['introduction'].widget.attrs.update({'class': 'form-control'})
+		self.fields['introduction'].required = False
+		self.fields['architecture'].widget.attrs.update({'class': 'form-control'})
+		self.fields['architecture'].required = False
+		self.fields['rituals'].widget.attrs.update({'class': 'form-control'})
+		self.fields['rituals'].required = False
+		self.fields['ceremonies'].widget.attrs.update({'class': 'form-control'})
+		self.fields['ceremonies'].required = False
+		self.fields['tales'].widget.attrs.update({'class': 'form-control'})
+		self.fields['tales'].required = False
+		self.fields['more_information'].widget.attrs.update({'class': 'form-control'})
+		self.fields['more_information'].required = False
+		# self.fields['image'].widget.attrs.update({'class': 'file', 'data-allowed-file-extensions':'["jpeg", "jpg","png"]', 'data-show-upload':'false', 'data-show-preview':'false', 'data-msg-placeholder':'Select article image for upload...'})
+		# self.fields['image'].required = False
 		self.fields['state'].widget.attrs.update({'class': 'form-control'})
 		self.fields['tags'].widget.attrs.update({'class': 'form-control'})
 		self.fields['tags'].required = False
@@ -30,8 +45,8 @@ class ArticleUpdateForm(forms.ModelForm):
 		self.fields['state'].queryset = States.objects.filter(name__in=states)
 		self.fields['state'].required = False
 
-		if not settings.REALTIME_EDITOR:
-			self.fields['body'].widget.attrs.update({'class': 'form-control'})
+		# if not settings.REALTIME_EDITOR:
+		# 	self.fields['body'].widget.attrs.update({'class': 'form-control'})
 
 class ArticleCreateForm(forms.ModelForm):
 	class Meta:
