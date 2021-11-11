667c09c38d8ed101d606668f8df97d54fa8efc8f
firuza <firya87@gmail.com>
2021-11-11 18:15:32 +0530

    display MediaStates table in django admin panel

--- Media/admin.py
+++ Media/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Media
+from .models import Media, MediaStates
 # Register your models here.
 
 admin.site.register(Media)
+admin.site.register(MediaStates)
