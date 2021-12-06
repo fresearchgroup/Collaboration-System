4923e045343331bf5b0d8210712cc7e856d70874
firuza <firya87@gmail.com>
2021-12-06 07:54:45 +0530

    modify admin for feedback

--- webcontent/admin.py
+++ webcontent/admin.py
@@ -3,6 +3,11 @@ from .models import Feedback, Faq, FaqCategory
 from django.db.models import Manager
 # Register your models here.
 
-admin.site.register(Feedback)
 admin.site.register(Faq)
 admin.site.register(FaqCategory)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'community', 'title', 'body', 'user', 'provided_at', ]
+    list_filter = ['community']
+    search_fields = ['community']
+admin.site.register(Feedback, FeedbackAdmin)
