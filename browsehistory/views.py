4e27c6021e7aada544e6743d92c40777ac80e48e
firuza <firya87@gmail.com>
2022-01-11 22:29:53 +0530

    display viewedon in browse history

--- browsehistory/views.py
+++ browsehistory/views.py
@@ -8,17 +8,28 @@ from Media.models import Media
 # Create your views here.
 def my_browse_history(request):
     if request.user.is_authenticated:
-        articlebrowsehistory = BrowseHistory.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(Articles))
+        articlebrowsehistory = BrowseHistory.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(Articles)).order_by('-viewed_on')
         articleids = []
         for history in articlebrowsehistory:
-            articleids.append(history.object_id)
-        recentarticles = CommunityArticles.objects.filter(article__pk__in=articleids)
+            if not any(history.object_id in sublist for sublist in articleids):
+                articleids.append([history.object_id, history.viewed_on])
+        recentarticles = CommunityArticles.objects.filter(article__pk__in=[i[0] for i in articleids])
+        for recent in recentarticles:
+            for id in articleids:
+                if recent.article.pk == id[0]:
+                    recent.accessedon = id[1]
 
-        mediabrowsehistory = BrowseHistory.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(Media))
+
+        mediabrowsehistory = BrowseHistory.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(Media)).order_by('-viewed_on')
         mediaids = []
         for history in mediabrowsehistory:
-            mediaids.append(history.object_id)
-        recentmedia = CommunityMedia.objects.filter(media__pk__in=mediaids)
+            if not any(history.object_id in sublist for sublist in mediaids):
+                mediaids.append([history.object_id, history.viewed_on])
+        recentmedia = CommunityMedia.objects.filter(media__pk__in=[i[0] for i in mediaids])
+        for recent in recentmedia:
+            for id in mediaids:
+                if recent.media.pk == id[0]:
+                    recent.accessedon = id[1]
 
         return render(request, 'mybrowsehistory.html', {'recentarticles':recentarticles, 'recentmedia':recentmedia})
     else:
